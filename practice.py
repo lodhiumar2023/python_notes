d9bdd719921a2f3627adb15cf87f92f86c271f0d
Umar Hayat Khan Lodhi <lodhiumar2023@gmail.com>
2026-02-21 19:52:25 +0500

    While Loop Notes Completed

---
+++ practice.py
@@ -0,0 +1,9 @@
+while True:
+    password = input("Enter password: ")
+    
+    if password == "python123":
+        print("Login successful")
+        break
+    else:
+        print("Wrong password")
+        
