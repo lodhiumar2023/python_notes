95ed368f52fd06d34734b8f28fcf4bb2469ab5c5
Umar Hayat Khan Lodhi <lodhiumar2023@gmail.com>
2026-02-22 17:51:27 +0500

    While Loop with exercise is completed

--- practice.py
+++ practice.py
@@ -1,9 +1,25 @@
+# Q14.
+# - Ask the user to enter numbers continuously.
+# - Stop when they enter "stop".
+# - At the end, print:
+
+# Total numbers entered \       length of list
+# Maximum Number \              list.max()
+# Minimum Number                list.min()
+
+
+userData = []
+
 while True:
-    password = input("Enter password: ")
-    
-    if password == "python123":
-        print("Login successful")
+    userInput = input("Please enter a number or stop to exit: ")
+
+    if userInput == "stop":
+        print("Thanks for using")
         break
     else:
-        print("Wrong password")
-        
+        userData.append(int(userInput))
+
+print("Total enter numbers are:", len(userData))
+print("Sum of number is:", sum(userData))
+print("Max of number is:", max(userData))
+print("Min of number is:", min(userData))
