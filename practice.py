81740082d8df9e4e3dcc06e62ebc9ccf4975f5ef
Umar Hayat Khan Lodhi <lodhiumar2023@gmail.com>
2026-02-24 22:27:07 +0500

    For loop questions solved some

--- practice.py
+++ practice.py
@@ -1,25 +1,5 @@
-# Q14.
-# - Ask the user to enter numbers continuously.
-# - Stop when they enter "stop".
-# - At the end, print:
-
-# Total numbers entered \       length of list
-# Maximum Number \              list.max()
-# Minimum Number                list.min()
+# 5. Loop with Condition (if inside for)
+# Print only even numbers from a list.
 
 
-userData = []
-
-while True:
-    userInput = input("Please enter a number or stop to exit: ")
-
-    if userInput == "stop":
-        print("Thanks for using")
-        break
-    else:
-        userData.append(int(userInput))
-
-print("Total enter numbers are:", len(userData))
-print("Sum of number is:", sum(userData))
-print("Max of number is:", max(userData))
-print("Min of number is:", min(userData))
+for x in
