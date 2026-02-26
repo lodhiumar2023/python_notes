097a0b5e3f1df6a644f41ddd35afffd115149792
Umar Hayat Khan Lodhi <lodhiumar2023@gmail.com>
2026-02-26 22:13:50 +0500

    For loop excercise updated

--- practice.py
+++ practice.py
@@ -1,5 +1,13 @@
-# 5. Loop with Condition (if inside for)
-# Print only even numbers from a list.
 
+numbers_list = [10, 36, 70, 49, 51]
 
-for x in
+max_num = numbers_list[0]
+
+for i in numbers_list:
+
+    if max_num < i:
+        print(max_num, i)  # Debugging
+        max_num = i
+        print("Max Number is i =", i)  # Debugging
+
+print(max_num)
