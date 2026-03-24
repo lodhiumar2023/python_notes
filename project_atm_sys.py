a8707ea4f46a9a7312f71cf5ec734c4c9802695e
Umar Hayat Khan Lodhi <lodhiumar2023@gmail.com>
2026-03-24 20:45:15 +0500

    Project ATM System completed

--- project_atm_sys.py
+++ project_atm_sys.py
@@ -25,6 +25,16 @@ main_menu = [
     "4. Exit"
 ]
 
+cash_draw_menu = [
+    "1. Rs.500",
+    "2. Rs.1000",
+    "3. Rs.2000",
+    "4. Rs.3000",
+    "5. Rs.4000",
+    "6. Rs.5000",
+    "7. Rs.10000"
+]
+
 
 # FUNCTION FOR PRINT LIST
 def print_list(items):
@@ -48,12 +58,44 @@ elif insert_card == "y":
 
     if menu_option == "4":
         print("Thanks you for using ATM Service.")
+
     elif menu_option == "1":
-        print("CASH WITHDRAWAL")  # --> Menu will be added
+        print_list(cash_draw_menu)
+        cash_option = input("Please select 1,2,3,4,5,6 or 7: ")
+
+        # CASH DRAW MENU
+        if cash_option == "1":
+            print("You draw Rs.500")
+            print("Thanks you for using ATM Service.")
+        elif cash_option == "2":
+            print("You draw Rs.1000")
+            print("Thanks you for using ATM Service.")
+        elif cash_option == "3":
+            print("You draw Rs.2000")
+            print("Thanks you for using ATM Service.")
+        elif cash_option == "4":t sta
+            print("You draw Rs.3000")
+            print("Thanks you for using ATM Service.")
+        elif cash_option == "5":
+            print("You draw Rs.4000")
+            print("Thanks you for using ATM Service.")
+        elif cash_option == "6":
+            print("You draw Rs.5000")
+            print("Thanks you for using ATM Service.")
+        elif cash_option == "7":
+            print("You draw Rs.10000")
+            print("Thanks you for using ATM Service.")
+        else:
+            print("Selected Wrong Option")
+
     elif menu_option == "2":
         print(balance)
+
+    # CHANGE PIN CODE
     elif menu_option == "3":
-        print("PLEASE ENTER YOUR NEW PIN")  # --> Menu will be added
+        change_pin_code = input("Please enter your new pin: ")
+        print("Your pin has been successfully changed!")
+
     else:
         print("Wrong Menu")
 
